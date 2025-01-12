aafd3fd99b9c2c2d026d158380a9dd4c008b202b
Augustine Joseph <augustine@Augustines-MacBook-Pro.local>
2025-01-12 15:50:10 +0530

    tailwind.config.js added

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const { withContentlayer } = require("next-contentlayer");
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',        // Static export
-  trailingSlash: true,     // Ensure URLs have a trailing slash
-};
-
-module.exports = withContentlayer(nextConfig);
+// next.config.js
+module.exports = {
+    reactStrictMode: true,
+    swcMinify: true, // optional for better minification
+  };
+  
