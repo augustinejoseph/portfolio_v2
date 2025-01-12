eab9b6ed023cba3271bdb6372e502f2c4eec20a7
Augustine Joseph <augustine@Augustines-MacBook-Pro.local>
2025-01-12 15:28:12 +0530

    removed contentlayer.config.ts file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',        // Static export
+  trailingSlash: true,     // Ensure URLs have a trailing slash
+};
 
 module.exports = withContentlayer(nextConfig);
